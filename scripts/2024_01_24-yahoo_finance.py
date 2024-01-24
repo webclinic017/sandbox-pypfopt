9391dfc51b59ac3ab1d09b8cb6f2eb59071fc11e
Luke Miloszewski <lukemiloszewski@gmail.com>
2024-01-24 14:38:53 +0200

    Add yahoo finance loader

--- scripts/2024_01_24-yahoo_finance.py
+++ scripts/2024_01_24-yahoo_finance.py
@@ -1,73 +1,61 @@
-import requests
-from bs4 import BeautifulSoup
 import re
+import time
 from datetime import datetime
-from time import mktime
+
+import pandas as pd
+import requests
 
 
-def load_csv_data(stock, interval='1d', day_begin='01-03-2018', day_end='28-03-2018'):
-    """
-    queries yahoo finance api to receive historical data in csv file format
-    
-    parameters: 
-        stock - short-handle identifier of the company
-        
-        interval - 1d, 1wk, 1mo - daily, weekly monthly data
-        
-        day_begin - starting date for the historical data (format: dd-mm-yyyy)
-        
-        day_end - final date of the data (format: dd-mm-yyyy)
-    
-    returns a list of comma seperated value lines
-    """
+def load_price_data(stock, interval="1d", day_begin="01-03-2019", day_end="28-03-2019"):
     day_begin_unix = _convert_to_unix(day_begin)
     day_end_unix = _convert_to_unix(day_end)
-    
     header, crumb, cookies = _get_crumbs_and_cookies(stock)
-    
-    with requests.session():
-        url = 'https://query1.finance.yahoo.com/v7/finance/download/' \
-              '{stock}?period1={day_begin}&period2={day_end}&interval={interval}&events=history&crumb={crumb}' \
-              .format(stock=stock, day_begin=day_begin_unix, day_end=day_end_unix, interval=interval, crumb=crumb)
-                
-        website = requests.get(url, headers=header, cookies=cookies)
-       
-        return website.text.split('\n')[:-1]
+
+    print(header, crumb, cookies)
+
+    url = f"https://query1.finance.yahoo.com/v7/finance/download/{stock}?period1={day_begin_unix}&period2={day_end_unix}&interval={interval}&events=history&crumb={crumb}"
+    response_raw = requests.get(url, headers=header, cookies=cookies)
+    response = response_raw.text.split("\n")[:-1]
+
+    rv = pd.DataFrame([sub.split(",") for sub in response])
+    print(rv)
+    return rv
 
 
 def _get_crumbs_and_cookies(stock):
-    """
-    get crumb and cookies for historical data csv download from yahoo finance
-    
-    parameters: stock - short-handle identifier of the company 
-    
-    returns a tuple of header, crumb and cookie
-    """
-    
-    url = 'https://finance.yahoo.com/quote/{}/history'.format(stock)
-    with requests.session():
-        header = {'Connection': 'keep-alive',
-                   'Expires': '-1',
-                   'Upgrade-Insecure-Requests': '1',
-                   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) \
-                   AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36'
-                   }
-        
-        website = requests.get(url, headers=header)
-        soup = BeautifulSoup(website.text, 'lxml')
-        crumb = re.findall('"CrumbStore":{"crumb":"(.+?)"}', str(soup))
+    # url = "https://query2.finance.yahoo.com/v1/test/getcrumb"  # doesn't work consistently
+    url = f"https://finance.yahoo.com/quote/{stock}/history"
+    header = {
+        "Connection": "keep-alive",
+        "Expires": "-1",
+        "Upgrade-Insecure-Requests": "1",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36",
+    }
 
-        return (header, crumb[0], website.cookies)
+
+    while True:
+        response = requests.get(url, headers=header)
+        if response.status_code != 200:
+            raise ConnectionError(f"Failed to retrieve data: HTTP {response.status_code}")
+
+        crumb_pattern = r'"crumb":"([a-zA-Z0-9]*)"'
+        crumb_match = re.search(crumb_pattern, response.text)
+
+        cookies = response.cookies
+
+        print(crumb_match, cookies)
+
+        if crumb_match:
+            crumb = crumb_match.group(1)
+            return header, crumb, cookies
+
+        time.sleep(1)
 
 
 def _convert_to_unix(date):
-    """
-    converts date to unix timestamp
-    
-    parameters: date - in format (dd-mm-yyyy)
-    
-    returns integer unix timestamp
-    """
-    datum = datetime.strptime(date, '%d-%m-%Y')
-    
-    return int(mktime(datum.timetuple()))
+    datum = datetime.strptime(date, "%d-%m-%Y")
+    return int(time.mktime(datum.timetuple()))
+
+
+if __name__ == "__main__":
+    load_price_data("MSFT")
